c37dc5029dca0ed4fecf5290e1959897ae49f86a
1droxion <patelsuchitbhai@gmail.com>
2025-05-29 19:49:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
-
 import Dashboard from "./Dashboard";
+import Generator from "./Generator";
 import AutoGenerator from "./AutoGenerator";
 import AIChat from "./AIChat";
 import AIImage from "./AIImage";
@@ -13,43 +13,41 @@ import Templates from "./Templates";
 import Connect from "./Connect";
 import Editor from "./Editor";
 import Profile from "./Profile";
-import Settings from "./Settings";
-import LandingPage from "./LandingPage";
-import Generator from "./Generator";
-import Analytics from "./Analytics";
 import Login from "./Login";
 import Signup from "./Signup";
+import Settings from "./Settings";
+import LandingPage from "./LandingPage";
+import Analytics from "./Analytics";
 
-function MainLayout() {
+function AppWrapper() {
   const location = useLocation();
-  const hideSidebarRoutes = ["/", "/login", "/signup"];
+
+  const hideSidebarRoutes = ["/login", "/signup"];
   const showSidebar = !hideSidebarRoutes.includes(location.pathname);
 
   return (
-    <div className="flex h-screen">
+    <div className="flex">
       {showSidebar && <Sidebar />}
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1">
         <Topbar />
-        <div className="p-4 flex-1 overflow-y-auto">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/autogenerator" element={<AutoGenerator />} />
-            <Route path="/generator" element={<Generator />} />
-            <Route path="/aichat" element={<AIChat />} />
-            <Route path="/aiimage" element={<AIImage />} />
-            <Route path="/plans" element={<Plans />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/templates" element={<Templates />} />
-            <Route path="/connect" element={<Connect />} />
-            <Route path="/editor" element={<Editor />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/generator" element={<Generator />} />
+          <Route path="/autogenerator" element={<AutoGenerator />} />
+          <Route path="/aichat" element={<AIChat />} />
+          <Route path="/aiimage" element={<AIImage />} />
+          <Route path="/plans" element={<Plans />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/templates" element={<Templates />} />
+          <Route path="/connect" element={<Connect />} />
+          <Route path="/editor" element={<Editor />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Routes>
       </div>
     </div>
   );
@@ -58,7 +56,7 @@ function MainLayout() {
 export default function App() {
   return (
     <Router>
-      <MainLayout />
+      <AppWrapper />
     </Router>
   );
 }
