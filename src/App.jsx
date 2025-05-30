5adbacf9d20b8add043501c656cfd6e76ba7a506
1droxion <patelsuchitbhai@gmail.com>
2025-05-29 20:13:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
-import LandingPage from "./LandingPage";
 import Dashboard from "./Dashboard";
 import Generator from "./Generator";
 import AutoGenerator from "./AutoGenerator";
-import AIImage from "./AIImage";
 import Chatboard from "./Chatboard";
+import AIImage from "./AIImage";
 import Plans from "./Plans";
 import Projects from "./Projects";
 import Templates from "./Templates";
@@ -16,22 +15,23 @@ import Connect from "./Connect";
 import Editor from "./Editor";
 import Profile from "./Profile";
 import Settings from "./Settings";
+import LandingPage from "./LandingPage";
 
 function App() {
   return (
     <Router>
-      <div className="flex h-screen bg-[#0e0e10] text-white">
+      <div className="flex min-h-screen bg-[#0e0e10] text-white">
         <Sidebar />
         <div className="flex-1 flex flex-col">
           <Topbar />
-          <main className="flex-1 overflow-y-auto p-4">
+          <div className="p-4">
             <Routes>
               <Route path="/" element={<LandingPage />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/generator" element={<Generator />} />
-              <Route path="/autogenerator" element={<AutoGenerator />} />
-              <Route path="/aiimage" element={<AIImage />} />
+              <Route path="/auto-generator" element={<AutoGenerator />} />
               <Route path="/chatboard" element={<Chatboard />} />
+              <Route path="/ai-image" element={<AIImage />} />
               <Route path="/plans" element={<Plans />} />
               <Route path="/projects" element={<Projects />} />
               <Route path="/templates" element={<Templates />} />
@@ -40,7 +40,7 @@ function App() {
               <Route path="/profile" element={<Profile />} />
               <Route path="/settings" element={<Settings />} />
             </Routes>
-          </main>
+          </div>
         </div>
       </div>
     </Router>
