3c11ac7b18dd7d173cef6c9e6ba0de6582d2747c
1droxion <patelsuchitbhai@gmail.com>
2025-06-04 20:32:36 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
-// src/api/axios.js
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, // 🔐 If needed for cookies/auth
+  withCredentials: true,
 });
 
 export default instance;
