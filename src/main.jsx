8486256a2ae7cf0667b446ea2d00854d7d2b7147
1droxion <patelsuchitbhai@gmail.com>
2025-06-08 00:17:53 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// ✅ main.jsx (Vite)
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import AppWrapper from "./App"; // changed name because App.jsx no longer exports <BrowserRouter>
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppWrapper />
+    </BrowserRouter>
   </React.StrictMode>
 );
