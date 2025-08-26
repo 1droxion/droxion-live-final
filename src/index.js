920d6f5b4f5c49e851c8a1b8456f666f2fe8063d
1droxion <patelsuchitbhai@gmail.com>
2025-08-26 15:08:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ export default function Home() {
   return (
     <main style={{ minHeight: '100vh', display: 'grid', placeItems: 'center', background: '#000', color: '#fff' }}>
       <div style={{ textAlign: 'center' }}>
-        <h1 style={{ fontFamily: 'system-ui, sans-serif', fontWeight: 700 }}>
-          Droxion
-        </h1>
-        <p>Next.js pages router is running.</p>
+        <h1 style={{ fontFamily: 'system-ui, sans-serif', fontWeight: 700 }}>Droxion</h1>
+        <p>Next.js is running on Render.</p>
       </div>
     </main>
   );
