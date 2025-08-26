1b59228ae672374eb729ca865af5553742a3084c
1droxion <patelsuchitbhai@gmail.com>
2025-08-26 15:03:21 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main style={{ minHeight: '100vh', display: 'grid', placeItems: 'center', background: '#000', color: '#fff' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ fontFamily: 'system-ui, sans-serif', fontWeight: 700 }}>
+          Droxion
+        </h1>
+        <p>Next.js pages router is running.</p>
+      </div>
+    </main>
+  );
+}
