78b8693976ef6cad305303754b6b2e025cf657cf
1droxion <patelsuchitbhai@gmail.com>
2025-09-11 16:03:17 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import AIChat from "./AIChat.jsx";
+import "./index.css"; // <-- important
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
