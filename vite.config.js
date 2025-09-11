187ae63caf7955d6c47c780325c2620e67ebf1ff
1droxion <patelsuchitbhai@gmail.com>
2025-09-11 16:45:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./",                     // <-- IMPORTANT for Capacitor
   plugins: [react()],
-  build: { outDir: "dist", sourcemap: false }
+  build: { outDir: "dist" }       // Capacitor uses the "dist" folder
 });
