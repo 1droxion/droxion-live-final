bb02a5110bb1faa1584f6efa9eefe0f1c5aae3b2
1droxion <patelsuchitbhai@gmail.com>
2025-09-11 15:49:40 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+import AIChat from "./AIChat.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AIChat />
   </React.StrictMode>
 );
