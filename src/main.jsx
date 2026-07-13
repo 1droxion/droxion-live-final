7915b662f958b0647a5c8bea85460642b20b5fde
1droxion <patelsuchitbhai@gmail.com>
2026-07-13 18:12:52 -0500

    Replace AIChat with BrowserRouter and App components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import AIChat from "./AIChat.jsx";
-import "./index.css"; // <-- important
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AIChat />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
