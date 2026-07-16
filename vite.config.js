408b916bc1f2764e29784d951f80df6a431e432a
1droxion <patelsuchitbhai@gmail.com>
2026-07-15 20:25:01 -0500

    Refactor vite.config.js for better formatting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",                     // <-- IMPORTANT for Capacitor
   plugins: [react()],
-  build: { outDir: "dist" }       // Capacitor uses the "dist" folder
+  build: {
+    outDir: "dist",
+  },
 });
