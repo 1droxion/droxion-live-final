909bba83411bc5a2edd767fc0022c81e0774670b
1droxion <patelsuchitbhai@gmail.com>
2026-08-23 22:01:49 -0500

    Enable global short feed playback recovery

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import ShortFeedPlaybackEnhancer from "./ShortFeedPlaybackEnhancer.jsx";
 import "./index.css";
 import "./responsive-overrides.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ShortFeedPlaybackEnhancer />
       <App />
     </BrowserRouter>
   </React.StrictMode>
