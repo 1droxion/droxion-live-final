946b1d5f4a2c7c82d5794d5ef94dbbb3ff470c7f
1droxion <patelsuchitbhai@gmail.com>
2026-08-26 17:33:21 -0500

    Remove LIVE startup race monkey patch

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./liveStartupRaceGuard.js";
 import App from "./App.jsx";
 import ShortFeedPlaybackEnhancer from "./ShortFeedPlaybackEnhancer.jsx";
 import LiveCameraStartupEnhancer from "./LiveCameraStartupEnhancer.jsx";
