987b6e1a4d75e1e28db13e3e6819b1599c8a03bb
1droxion <patelsuchitbhai@gmail.com>
2026-08-26 16:51:00 -0500

    Mount LIVE camera startup recovery

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import ShortFeedPlaybackEnhancer from "./ShortFeedPlaybackEnhancer.jsx";
+import LiveCameraStartupEnhancer from "./LiveCameraStartupEnhancer.jsx";
 import "./index.css";
 import "./responsive-overrides.css";
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ShortFeedPlaybackEnhancer />
+      <LiveCameraStartupEnhancer />
       <App />
     </BrowserRouter>
   </React.StrictMode>
