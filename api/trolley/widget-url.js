04b2b425fa9e6323160ec9e4bfc043fc5dc28576
1droxion <patelsuchitbhai@gmail.com>
2026-09-02 18:20:50 -0500

    Fix CORS for Trolley bank setup

--- api/trolley/widget-url.js
+++ api/trolley/widget-url.js
@@ -1,8 +1,22 @@
 import { buildTrolleyWidgetUrl, getSupabaseUser } from '../../server/trolley-lib.js';
 
+function setCorsHeaders(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  res.setHeader('Access-Control-Max-Age', '86400');
+}
+
 export default async function handler(req, res) {
+  setCorsHeaders(res);
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
-    res.setHeader('Allow', 'POST');
+    res.setHeader('Allow', 'POST, OPTIONS');
     res.status(405).json({ error: 'Method not allowed.' });
     return;
   }
