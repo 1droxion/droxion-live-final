bb55cf09eb6dc55d10bd66daced0cff96ba8e2e5
1droxion <patelsuchitbhai@gmail.com>
2026-09-07 13:50:47 -0500

    Enable native external LIVE player bridge

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import ShortFeedPlaybackEnhancer from "./ShortFeedPlaybackEnhancer.jsx";
 import LiveCameraStartupEnhancer from "./LiveCameraStartupEnhancer.jsx";
 import AdReadyEnhancer from "./AdReadyEnhancer.jsx";
+import NativeExternalLivePlayerBridge from "./NativeExternalLivePlayerBridge.jsx";
 import "./index.css";
 import "./responsive-overrides.css";
 import "./profile-polish.css";
@@ -12,13 +13,15 @@ import "./navigation-v2.css";
 import "./creator-auto-video.css";
 
 const isLiveV2Path = window.location.pathname.startsWith('/live-v2');
+const isNativeLivePlayerPath = window.location.pathname === '/native-live-player';
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {!isLiveV2Path && <ShortFeedPlaybackEnhancer />}
-      {!isLiveV2Path && <LiveCameraStartupEnhancer />}
-      {!isLiveV2Path && <AdReadyEnhancer />}
+      {!isLiveV2Path && !isNativeLivePlayerPath && <ShortFeedPlaybackEnhancer />}
+      {!isLiveV2Path && !isNativeLivePlayerPath && <LiveCameraStartupEnhancer />}
+      {!isLiveV2Path && !isNativeLivePlayerPath && <AdReadyEnhancer />}
+      {!isNativeLivePlayerPath && <NativeExternalLivePlayerBridge />}
       <App />
     </BrowserRouter>
   </React.StrictMode>
